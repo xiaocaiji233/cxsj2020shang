37bbbcfbbb4785e2c46b09d6ad76b9f8ec91dec0
KOISHI <386098423@outlook.com>
2020-05-13 02:28:00 +0800

    2020/05/13_wjw_week12_corrected_version_

--- 1901_wjw/2020513/week12/week12.py
+++ 1901_wjw/2020513/week12/week12.py
@@ -4,7 +4,9 @@ import time
 import gensim
 import multiprocessing
 import jieba.posseg as psg
-from gensim.models import KeyedVectors,word2vec,Word2Vec
+from gensim.models import KeyedVectors, word2vec, Word2Vec
+import pprint
+
 jieba.load_userdict('stop_words.txt')
 
 
@@ -19,27 +21,30 @@ sentences_cut = []
 f = open('test.txt', encoding='utf-8')  # 打开中文文档
 paragraph = f.read()
 # 结巴分词
-for ele in paragraph:
-    cuts = jieba.cut(ele, cut_all=False)
-    new_cuts = []
-    for cut in cuts:
-        if cut not in stopwords:
-            new_cuts.append(cut)
-    res = ' '.join(new_cuts)
-    sentences_cut.append(res)
+
+cuts = jieba.cut(paragraph, cut_all=False)
+# pprint.pprint(cuts)
+new_cuts = []
+for cut in cuts:
+    if cut not in stopwords:
+        new_cuts.append(cut)
+        # pprint.pprint(cut)
+# pprint.pprint(stopwords)
+res = ' '.join(new_cuts)
+sentences_cut.append(res)
+
 
 with open('sentence.txt', 'w', encoding='utf-8') as f:
     for ele in sentences_cut:
         ele = ele + '\n'
         f.write(ele)
 
+sentences = list(word2vec.LineSentence('sentence.txt'))\
 
-sentences = list(word2vec.LineSentence('sentence.txt'))
-
-
+pprint.pprint(sentences)
 # 训练方式1
 model = Word2Vec(sentences, size=256, min_count=1, window=5, sg=0, workers=multiprocessing.cpu_count())
-print(model)
+# print(model)
 
 model.save('word2vec.model')
 model.wv.save_word2vec_format('word2vec.vector')
@@ -47,5 +52,5 @@ model.wv.save_word2vec_format('word2vec.vector')
 t1 = time.time()
 model = Word2Vec.load('word2vec.model')
 t2 = time.time()
-print(model)
-print(".model load time %.4f"%(t2-t1))
+# print(model)
+# print(".model load time %.4f" % (t2 - t1))
